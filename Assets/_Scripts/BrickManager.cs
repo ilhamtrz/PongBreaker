9aa8b85536c55555aba9db75d7025e6ed83f5a28
Dimaspadma <dimaspadma@gmail.com>
2022-11-04 16:36:23 +0700

    Update brick and brickManager

--- Assets/_Scripts/BrickManager.cs
+++ Assets/_Scripts/BrickManager.cs
@@ -1,66 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BrickManager : MonoBehaviour
 {
-    [Header("Box Size")] 
-    public float xSize = 1;
-    public float ySize = 1;
-
-    [Header("Prefab Brick")] 
-    public GameObject brick;
-    public Vector2 offset;
-
-    private Vector2 _brickSize;
+    public List<Vector2> brickDestroyed;
 
     private void Start()
     {
-        _brickSize = brick.transform.localScale;
-        GenerateBrick();
+        brickDestroyed = new List<Vector2>();
     }
 
-    private void OnDrawGizmos()
-    {
-        var center = transform.position;
-        center.x += xSize / 2;
-        center.y += ySize / 2;
-        
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(center, new Vector3(xSize,ySize,1));
-    }
-
-    void GenerateBrick()
-    {
-        Vector3 newPos = Vector3.zero;
-
-        var n = new Vector2(0,0);
-        while (newPos.x < xSize)
-        {
-            newPos.x += _brickSize.x / 2;
-            
-            while (newPos.y < ySize)
-            {
-                newPos.y += _brickSize.y / 2;
-
-                if (newPos.y >  ySize || newPos.x > xSize)
-                {
-                    continue;
-                }   
-                
-                var spawned = Instantiate(brick, transform);
-                spawned.transform.position += newPos;
-                spawned.name = "Brick " + n.x + ","+ n.y;
-
-                newPos.y += _brickSize.y / 2 + offset.y;
-                n.y++;
-            }
-
-            newPos.y = 0;
-            n.y = 0;
-            newPos.x += _brickSize.x / 2 + offset.x;
-            n.x++;
-        }
-    }
 }
