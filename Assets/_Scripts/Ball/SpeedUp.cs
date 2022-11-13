5fec9287504ac6e2531eb8762d111ba6d754d809
Dimaspadma <dimaspadma@gmail.com>
2022-11-13 17:26:50 +0700

    Update trail ball time

--- Assets/_Scripts/Ball/SpeedUp.cs
+++ Assets/_Scripts/Ball/SpeedUp.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class SpeedUp : MonoBehaviour
 {
+    public GameObject trailObj;
+    private TrailRenderer trail;
+    
     public float speedUp = 0;
+    public float trailWidthStart = 0.01f;
+    public float trailWidthUp = 0.01f;
 
     private Rigidbody2D _rb;
     private Vector2 velocityStart;
@@ -14,6 +19,8 @@ public class SpeedUp : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        trail = trailObj.GetComponent<TrailRenderer>();
+        trail.time = trailWidthStart;
     }
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -27,11 +34,13 @@ public class SpeedUp : MonoBehaviour
         if (col.collider.CompareTag("Paddle"))
         {
             _rb.velocity *= 1 + speedUp;
+            trail.time += trailWidthUp;
         }
 
         if (col.collider.CompareTag("Brick"))
         {
             _rb.velocity = velocityStart;
+            trail.time = trailWidthStart;
             resetVelocity = true;
         }
     }
