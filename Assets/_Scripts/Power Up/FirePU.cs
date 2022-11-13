f7ab7a2b101b1485656a38870dfec53954511f4a
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2022-11-13 20:09:12 +0700

    powerup spawning done

---
+++ Assets/_Scripts/Power Up/FirePU.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePU : MonoBehaviour
+{
+    public PowerUpManager manager;
+    public GameObject paddle1;
+    public GameObject paddle2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision == paddle1 || collision == paddle2)
+        {
+            manager.RemovePowerUp(gameObject);
+        }
+    }
+}
