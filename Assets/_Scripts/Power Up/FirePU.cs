26e8c29e03fe6a12901740b2735e37dd1882ca82
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2022-11-29 13:32:42 +0700

    edit powerupManager

--- Assets/_Scripts/Power Up/FirePU.cs
+++ Assets/_Scripts/Power Up/FirePU.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FirePU : MonoBehaviour
 {
-    public PowerUpManager manager;
+    public PowerUpManagerNew manager;
     public GameObject paddle1;
     public GameObject paddle2;
 
--- Assets/_Scripts/Power Up/IcePU.cs
+++ Assets/_Scripts/Power Up/IcePU.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class IcePU : MonoBehaviour
 {
-    public PowerUpManager manager;
+    public PowerUpManagerNew manager;
     public GameObject paddle1;
     public GameObject paddle2;
 
