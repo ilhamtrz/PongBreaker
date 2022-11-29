1405c2a2c46913a194ce58e1c13dd0c5b0e5768b
Dimaspadma <dimaspadma@gmail.com>
2022-11-29 16:11:45 +0700

    testing

--- Assets/_Scripts/Paddle/Dash2.cs
+++ Assets/_Scripts/Paddle/Dash2.cs
@@ -3,14 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dash : MonoBehaviour
+public class Dash2 : MonoBehaviour
 {
     [Header("Key Code")]
     public KeyCode key;
 
-    [Header("Paddle control")] 
-    public Transform sprite;
-    public Transform root;
+    [Header("Paddle control")]
     public float dashDistance;
 
     [Header("Movement")] 
@@ -21,51 +19,64 @@ public class Dash : MonoBehaviour
     public float backSpeed;
     public bool collide = false;
 
-    [Header("VFX")] public ParticleSystem trail;
-    
     private Vector2 _startPos, _destPos;
     private Vector3 _startScale;
     private bool canDash = true;
 
     public bool dash = false;
+    private Rigidbody2D rb;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        _startPos = transform.position;
+        _destPos = _startPos + (Vector2)transform.right * dashDistance;
+    }
 
     private void Update()
     {
-        _startPos = root.position;
-        _destPos = _startPos + (Vector2)transform.right * dashDistance;
+        
 
         if (Input.GetKeyDown(key) && canDash)
         {
-            trail.Play();
+            _startPos = transform.position;
+            _destPos = _startPos + (Vector2)transform.right * dashDistance;
+            
             canDash = false;
             StartCoroutine(DashForward());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (dash)
+        {
+            rb.AddForce(transform.right * 2f);
         }
     }
 
     IEnumerator DashForward()
     {
         // Check if the position of the cube and sphere are approximately equal.
-        while (Vector2.Distance(sprite.position, _destPos) > 0.001f && !collide)
+        while (Vector2.Distance(transform.position, _destPos) > 0.001f && !collide)
         {
             dash = true;
             // Paddle
-            var step = forwardSpeed * Time.deltaTime;
-            sprite.position = Vector2.MoveTowards(sprite.position, _destPos, forwradCurve.Evaluate(step));
+            // var step = forwardSpeed * Time.deltaTime;
+            // sprite.position = Vector2.MoveTowards(sprite.position, _destPos, forwradCurve.Evaluate(step));
 
             yield return null;
         }
-
-        trail.Stop();
         yield return StartCoroutine(DashBack());
     }
 
     IEnumerator DashBack()
     {
-        while (Vector2.Distance(sprite.position, _startPos) > 0.001f)
+        while (Vector2.Distance(_startPos, _destPos) > 0.001f)
         {
             dash = false;
             var step = backSpeed * Time.deltaTime;
-            sprite.position = Vector2.MoveTowards(sprite.position, _startPos, backCurve.Evaluate(step));
+            transform.position = Vector2.MoveTowards(_destPos, _startPos, backCurve.Evaluate(step));
 
             yield return null;
         }
